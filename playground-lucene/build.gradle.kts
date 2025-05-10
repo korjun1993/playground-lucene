4b2a6150a5b981115339b1594ad538f6e62ff969
korjun1993 <corojoon93@naver.com>
2025-05-10 15:37:51 +0900

    [build] Add lucene dependency

--- playground-lucene/build.gradle.kts
+++ playground-lucene/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.apache.lucene/lucene-core
+    implementation("org.apache.lucene:lucene-core:3.0.0")
 }
 
 tasks.test {
