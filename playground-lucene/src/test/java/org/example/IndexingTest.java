ca0403274a3df211aa0621a4cf033e95ee336edd
김호준 <hj.kim@joonggonara.co.kr>
2025-07-01 19:38:50 +0900

    [test] delete documents

--- playground-lucene/src/test/java/org/example/IndexingTest.java
+++ playground-lucene/src/test/java/org/example/IndexingTest.java
@@ -9,6 +9,7 @@ import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriter.MaxFieldLength;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.junit.jupiter.api.Assertions;
@@ -53,10 +54,35 @@ public class IndexingTest {
 
     @Test
     @DisplayName("Reader 테스트")
-    public void test() throws IOException {
+    public void test2() throws IOException {
         IndexReader reader = IndexReader.open(directory);
         Assertions.assertEquals(reader.numDocs(), ids.length);
         Assertions.assertEquals(reader.maxDoc(), ids.length);
         reader.close();
+    }
+
+    @Test
+    @DisplayName("삭제 테스트")
+    public void test3() throws IOException {
+        IndexWriter writer = getWriter();
+        writer.deleteDocuments(new Term("id", "1"));
+        writer.commit();
+        Assertions.assertTrue(writer.hasDeletions()); // 색인에 삭제됐다고 표시된 문서가 있는지 확인
+        Assertions.assertEquals(2, writer.maxDoc()); // 삭제 여부에 관계없이 maxDoc은 전체 문서 수를 나타냄
+        Assertions.assertEquals(1, writer.numDocs()); // 삭제되지 않은 문서 수를 나타냄
+        writer.close();
+    }
+
+    @Test
+    @DisplayName("삭제후 최적화 테스트")
+    public void test4() throws IOException {
+        IndexWriter writer = getWriter();
+        writer.deleteDocuments(new Term("id", "1"));
+        writer.optimize();
+        writer.commit();
+        Assertions.assertFalse(writer.hasDeletions()); // 최적화 후 삭제된 문서가 없음을 확인
+        Assertions.assertEquals(1, writer.maxDoc()); // 최적화를 진행했기 때문에 maxDoc 메소드 역시 2 대신 1 값을 리턴
+        Assertions.assertEquals(1, writer.numDocs());
+        writer.close();
     }
 }
